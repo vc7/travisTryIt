d241f3d8929d5d8c7a503d48d8936478a344e750
vc7 <vince78718@gmail.com>
2016-10-23 23:14:24 +0800

    Initial Commit

---
+++ travisTryIt/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  travisTryIt
+//
+//  Created by vincent on 2016/10/23.
+//  Copyright © 2016年 vc7. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
